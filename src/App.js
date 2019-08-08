82297bdedc0a1e05324bdc663a32bc1f44cd4fe8
Stephen McCullough <stephen.mccullough@gmail.com>
2019-08-08 23:59:31 +0100

    Added nav component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import {Department} from './components/Department'
 import {Employee} from './components/Employee' 
 import {Home} from './components/Home'
+import {Navigation} from './components/Navigation'
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import NavLink from 'react-bootstrap/NavLink';
 
 function App() {
   return (
     
     <BrowserRouter>
       <div className="container">
+
+        <h3 className="m-3 d-flex justify-content-center">
+          React JS with Web api Demo
+        </h3>
+
+        <h5 className="m-3 d-flex justify-content-center">
+          Employee Management Portal
+        </h5>
+
+        <Navigation/>
+
+
+
         <Switch>
           <Route path='/' component={Home} exact />
           <Route path='/department' component={Department} />
