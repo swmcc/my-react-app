cc36ca9fda90ea1fc9d32fe550bc5885a1e0529e
Stephen McCullough <stephen.mccullough@gmail.com>
2019-08-09 01:46:00 +0100

    Now gets data from api end point

--- src/components/Department.js
+++ src/components/Department.js
@@ -1,13 +1,47 @@
 import React,{Component} from 'react'
+import {Table} from 'react-bootstrap'
 
 export class Department extends Component {
 
+    constructor(props){
+        super(props)
+        this.state = {deps:[]}
+    }
+
+    componentDidMount(){
+        this.refreshList()
+    }
+
+    refreshList() {
+        fetch('http://localhost:3000/departments')
+        .then(response => response.json())
+        .then(data => { 
+            this.setState({deps: data})
+        })    
+    }
+
     render(){
+
+        const {deps} = this.state
+
         return(
-            <div className="mt-5 d-flex justify-content-left">
-                <h3>Welcome to Employee Management Portal. 
-                    This is the Department page.</h3>
-            </div>
+            <Table className="mt-4" striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {deps.map(dep => 
+                         <tr key = {dep.id}>
+                         <td> {dep.id} </td>
+                         <td> {dep.name} </td>
+                         </tr>
+
+                        )}
+                </tbody>
+            </Table>
         )
     }
 }
